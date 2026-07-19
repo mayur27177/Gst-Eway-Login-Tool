19e142e4d7c1e9a7a4cbd70e847af5a75ffebf9a
mayur27177 <mayurgupta9829927177@gmail.com>
2026-07-20 02:08:38 +0530

    Add files via upload

---
+++ visitor-counter.js
@@ -0,0 +1,2 @@
+/* Samriddhi Associates website visitor counter endpoint. */
+window.VISITOR_COUNT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxGLtIXkWxPfMjocbL2Siel3s4dZPr2QjBpChMO-MCGU4LoX_ZrCYTtzva_k5cWXfaG/exec";
